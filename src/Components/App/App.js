1ba2708c512bbb3c0940d6cbed995e3e5df5c825
Lebegue Guillaume <guillaume_lebegue@ens.univ-artois.fr>
2019-03-29 10:29:21 +0100

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+//import logo from '../../assets/img/logo.svg'
+import '../../assets/css/App.css'
+
+import Routes from '../Routes/Routes'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Bon courage ;-)</h1>
+        <Routes />
+      </div>
+    )
+  }
+}
+
+export default App
